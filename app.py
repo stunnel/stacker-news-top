b7d21bc5a50223f8b4c645367392c7fa613fa5ed
Travis Lee <travislee8964@users.noreply.github.com>
2022-11-11 13:12:51 +0000

    change to BackgroundScheduler and run immediately

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from apscheduler.schedulers.blocking import BlockingScheduler
+import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
 from stackernews import StackerNews
 
 stacker = StackerNews()
 
-scheduler = BlockingScheduler(timezone='Asia/Hong_Kong')
-scheduler.add_job(stacker.run, 'interval', minutes=10, id='stacker')
+scheduler = BackgroundScheduler(timezone='Asia/Hong_Kong')
+scheduler.add_job(stacker.run, 'interval', minutes=10, id='stacker', next_run_time=datetime.datetime.now())
 
 
 if __name__ == '__main__':
