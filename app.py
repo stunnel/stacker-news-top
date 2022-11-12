d19f31b0e1e485a6e655e4d86c36bb17ca1d5c69
Travis Lee <travislee8964@users.noreply.github.com>
2022-11-12 04:58:49 +0000

    fix: No need to specify a timezone

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from stackernews import StackerNews
 
 stacker = StackerNews()
 
-scheduler = BlockingScheduler(timezone='Asia/Hong_Kong')
+scheduler = BlockingScheduler()
 scheduler.add_job(stacker.run, 'interval', minutes=1, id='stacker',
                   max_instances=1, next_run_time=datetime.datetime.now())
 
